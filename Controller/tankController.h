5aadaa651366fa59bc41a689b19e7fbe45d38ad7
Stanislav Kitov <s_kitov@tu-sofia.bg>
2016-06-09 11:14:48 +0300

    Init Tank controller repository

---
+++ Controller/tankController.h
@@ -0,0 +1,9 @@
+#ifndef __TANKCONTROLLER_H
+#define __TANKCONTROLLER_H
+
+#include "definitions.h"
+
+#define LCD_REFRESH_TIMER                       TIMER_1
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,22 @@
+#include "stm32f4xx.h"
+#include "stm32f4xx_conf.h"
+#include "system_stm32f4xx.h"
+#include "definitions.h"
+#include "rcc.h"
+#include "initPeripheral.h"
+#include "userLibrary.h"
+#include "VTimer.h"
+#include "usart.h"
+#include "mytim.h"
+#include "serial.h"
+#include "mbslave.h"
+#include "rs232.h"
+#include "LCD.h"
+
+
+
+
+int main()
+{
+    return 0;
+}
